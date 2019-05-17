c7b88c216bff4f1d972d6ff25c9323ba209e1164
Your Name <you@example.com>
2019-05-17 12:57:41 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require "pry"
 class Transfer
   # your code here
+  attr_accessor :amount, :sender, :receiver, :status
+
+  def initialize (sender , receiver , amount)
+
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status =  "pending"
+
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if @sender.balance - @amount > 0 && self.status == "pending"
+      @receiver.balance += amount
+      @sender.balance -= amount
+      return self.status = "complete"
+
+    elsif  @sender.balance - @amount < 0
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @receiver.balance -= amount
+      @sender.balance += amount
+      return self.status = "reversed"
+    end
+  end
 end
